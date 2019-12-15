4142d23f16c7f00e4168e3c465f78f5e8b7c822c
Annie Liao <liaowow@gmail.com>
2019-12-15 13:33:05 -0500

    Mission accomplished

--- app/models/funding_round.rb
+++ app/models/funding_round.rb
@@ -1,3 +1,38 @@
 class FundingRound
 
+    attr_reader :startup, :venture_capitalist
+    attr_accessor :type, :invest_amount
+
+    @@all = []
+
+    def initialize(startup, venture_capitalist, type, invest_amount)
+        @startup = startup
+        @venture_capitalist = venture_capitalist
+        @type = type
+        @invest_amount = invest_amount
+        @@all << self
+    end
+
+    def startup
+        @startup
+    end
+
+    def venture_capitalist
+        @venture_capitalist
+    end
+
+    def type
+        @type
+    end
+
+    def investment
+        @invest_amount
+    end
+
+    def self.all
+        @@all
+    end
+
 end
+
+
--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,3 +1,87 @@
 class Startup
+    attr_accessor :startup_name, :startup_domain
+    attr_reader :founder_name
+
+    @@all = []
+
+    def initialize(startup_name, founder_name, startup_domain)
+        @startup_name = startup_name
+        @founder_name = founder_name
+        @startup_domain = startup_domain
+        @@all << self
+    end
+
+    def domain
+        @startup_domain
+    end
+
+    def pivot(domain, name)
+        @startup_domain = domain
+        @startup_name = name
+    end
+
+    # Startup.find_by_founder
+    # given a string of a founder's name, returns the first startup whose founder's name matches
+    def find_by_founder(founder)
+        @@all.find do |startup_instance|
+            startup_instance.founder_name == founder
+        end
+    end
+
+    # Startup.domains
+    # should return an array of all of the different startup domains
+    def domains
+        @@all.map do |startup_instance|
+            startup_instance.startup_domain
+        end
+    end
+
+    def sign_contract(vc_object, investment_type, investment_amount)
+        FundingRound.new(self, vc_object, investment_type, investment_amount)
+    end
+
+    def num_funding_rounds
+        num = 0
+        FundingRound.all.each do |round_object|
+            if round_object.startup == self
+                num += 1
+            end
+        end
+        num
+    end
+
+    def total_funds
+        sum = 0
+        FundingRound.all.each do |round_object|
+            if round_object.startup == self
+                sum += round_object.invest_amount
+            end
+        end
+        sum
+    end
+
+    def investors
+        result = FundingRound.all.map{|round_object|round_object.venture_capitalist if round_object.startup == self}.uniq.compact
+    end
+
+    def big_investors
+        # 1st loop: loop thru the array of rich VCs (VCs that are worth > 1 bil)
+        # 2nd loop: loop thru those invested in this startup (by calling #investors method)
+        # check if any of rich VCs is the same VCs that invested in this startup
+        # if so, push that object to an empty array
+        result = []
+        VentureCapitalist.tres_commas_club.map do |rich_vc_object|
+            investors.map do |vc_object|
+                if rich_vc_object == vc_object
+                    result << rich_vc_object
+                end
+            end
+        end
+        result
+    end
+
+    def self.all
+        @@all
+    end
 
 end
