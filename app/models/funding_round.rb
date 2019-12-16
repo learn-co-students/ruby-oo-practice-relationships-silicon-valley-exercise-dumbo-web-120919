f93e0a6261f12d793fa725ee03b43aea190da452
hylobates-lar <alison.v.quaglia@gmail.com>
2019-12-16 18:57:10 -0500

    all tests passing

--- app/models/funding_round.rb
+++ app/models/funding_round.rb
@@ -1,3 +1,23 @@
 class FundingRound
+    attr_reader :startup, :venture_capitalist
+    attr_accessor :type, :investment   
+
+    @@all = []
+
+    def initialize(startup, venture_capitalist, type, investment)
+        @startup = startup
+        @venture_capitalist = venture_capitalist 
+        @type = type
+        # Examples include: Angel, Pre-Seed, Seed, Series A, Series B, Series C, etc.
+        @investment = investment
+        # This should be a float that is not a negative number.
+
+        @@all << self
+    end
+
+    def self.all 
+        @@all 
+    end
 
 end
+
